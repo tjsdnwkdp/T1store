0ea2c0142c3f18c25b3a327c5ce0e06968a485a0
tjsdnwkdp <0811csw@naver.com>
2024-08-22 19:17:21 +0900

    first commit

---
+++ src/main/java/com/T1store/T1store/T1storeApplication.java
@@ -0,0 +1,13 @@
+package com.T1store.T1store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1storeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1storeApplication.class, args);
+	}
+
+}
